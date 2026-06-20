99527164a9a1cca1e0d4563e9cb2d77f9db92eaf
albertgmi <gmitrzak.albert@gmail.com>
2026-06-20 10:39:12 +0200

    user field migration added

---
+++ inzBackend/Migrations/20260620083852_newColumnForUserAdded.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace inzBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class newColumnForUserAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DoublePointsExpiresAt",
+                table: "Users",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 10, 38, 49, 613, DateTimeKind.Unspecified).AddTicks(6022), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 10, 38, 49, 613, DateTimeKind.Unspecified).AddTicks(6134), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 10, 38, 49, 613, DateTimeKind.Unspecified).AddTicks(6139), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 10, 38, 49, 613, DateTimeKind.Unspecified).AddTicks(6149), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 10, 38, 49, 613, DateTimeKind.Unspecified).AddTicks(6156), new TimeSpan(0, 2, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DoublePointsExpiresAt",
+                table: "Users");
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 19, 21, 39, 22, 239, DateTimeKind.Unspecified).AddTicks(1871), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 19, 21, 39, 22, 239, DateTimeKind.Unspecified).AddTicks(2032), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 19, 21, 39, 22, 239, DateTimeKind.Unspecified).AddTicks(2037), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 19, 21, 39, 22, 239, DateTimeKind.Unspecified).AddTicks(2049), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 19, 21, 39, 22, 239, DateTimeKind.Unspecified).AddTicks(2061), new TimeSpan(0, 2, 0, 0, 0)));
+        }
+    }
+}
