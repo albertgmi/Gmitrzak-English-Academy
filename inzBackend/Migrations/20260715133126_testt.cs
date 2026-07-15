b0bc646ead276ca17a069ccfaac8f60c7872ef9d
albertgmi <gmitrzak.albert@gmail.com>
2026-07-15 15:37:33 +0200

    fixes in migrations

---
+++ inzBackend/Migrations/20260715133126_testt.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace inzBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class testt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 7, 15, 15, 31, 24, 155, DateTimeKind.Unspecified).AddTicks(6920), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 7, 15, 15, 31, 24, 155, DateTimeKind.Unspecified).AddTicks(7071), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 7, 15, 15, 31, 24, 155, DateTimeKind.Unspecified).AddTicks(7076), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 7, 15, 15, 31, 24, 155, DateTimeKind.Unspecified).AddTicks(7090), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 7, 15, 15, 31, 24, 155, DateTimeKind.Unspecified).AddTicks(7098), new TimeSpan(0, 2, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 12, 26, 44, 160, DateTimeKind.Unspecified).AddTicks(1879), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 12, 26, 44, 160, DateTimeKind.Unspecified).AddTicks(1996), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 12, 26, 44, 160, DateTimeKind.Unspecified).AddTicks(2000), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 12, 26, 44, 160, DateTimeKind.Unspecified).AddTicks(2012), new TimeSpan(0, 2, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ShopItems",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 6, 20, 12, 26, 44, 160, DateTimeKind.Unspecified).AddTicks(2020), new TimeSpan(0, 2, 0, 0, 0)));
+        }
+    }
+}
